cb729bcfd12c655ce4769005f2f012d8e5041045
George Luke <george.luke92@gmail.com>
2018-02-11 16:25:19 -0600

    all the things

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+$(function(){
+
+  //create newItem out of submission
+  function handleNewItemSubmit() { 
+    $('.').on('submit', event => {
+      event.preventDefault();
+      const newItem = $('.js-shopping-list-entry').val();
+      $('.js-shopping-list-entry').val('');
+    });
+    return newItem;
+}
+
+//make a new item with data submitted
+  function makeNewItem() {
+    $('ul').on('submit', event => {
+        $('ul').append(`<li>
+                <span class="shopping-item">`${newItem}`</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`)
+    });
+
+  );
